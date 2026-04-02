642407e8f887de519fa2c78af8717c4da3d02d38
Tostenn <kouyatosten@gmail.com>
2026-04-02 13:24:10 +0200

    refactor: mise à jour de package.json et configuration Vite pour une meilleure structure de projet

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,30 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import { resolve } from "path";
 
-export default defineConfig(({ command, mode }) => {
-
-  return {
-    base: '/TechCard/',
-    build: {
-      // Configuration de la Librairie (pour le CDN)
-      lib: {
-        entry: resolve(__dirname, "src/index.ts"),
-        name: "TechCard",
-        fileName: (format) => (format === "umd" ? `techcard.js` : `techcard.mjs`),
-        formats: ["umd", "es"],
-      },
-      rollupOptions: {
-        // Si on build le site, on a besoin de l'index.html
-        // Si on build la lib, Rollup s'occupe de l'entry point défini plus haut
-        input: {
-          main: resolve(__dirname, 'index.html'),
-        },
-        output: {
-          exports: "named",
-          extend: true,
-        },
-      },
-      minify: "terser",
-      // On laisse la commande npm écraser l'outDir
-      outDir: "dist", 
+export default defineConfig({
+  base: '/TechCard/',
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "TechCard",
+      fileName: (format) => (format === "umd" ? `techcard.js` : `techcard.mjs`),
+      formats: ["umd", "es"],
     },
-    plugins: [dts()],
-  };
+    rollupOptions: {
+      output: {
+        exports: "named",
+        extend: true,
+      },
+    },
+    minify: "terser",
+    outDir: "dist", 
+  },
+  plugins: [
+    dts({ 
+      insertTypesEntry: true, // Ajoute automatiquement le champ types dans le package.json (si absent)
+      rollupTypes: true,      // Fusionne tous les .d.ts en un seul index.d.ts
+    })
+  ],
 });
